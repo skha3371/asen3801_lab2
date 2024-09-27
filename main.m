756eb3c151c7e63084a0d60b462bcc2e14e34cb9
skha3371 <skylar.harris@colorado.edu>
2024-09-26 22:50:10 -0600

    comments on code

--- main.m
+++ main.m
@@ -5,10 +5,12 @@
 
 clc;clear;close all;
 
+% using the LoadASPENDATA function to get the data
 [t_vec,av_pos_inert,av_att,tar_pos_inert,tar_att] = LoadASPENData("Section003_GroupA_Data");
 
 %% Question 3
 
+% plotting the 3d position in the n-frame
 figure
 plot3(av_pos_inert(1,:),-av_pos_inert(2,:),-av_pos_inert(3,:),'b')
 hold on
@@ -23,6 +25,7 @@ legend('Path of the Aerospace Vehicle','Path of the Target')
 
 %% Question 4
 
+% sublot of the position of both objects in the e-frame
 figure
 sgtitle('Position vs Time for Both Objects in the E-Frame')
 
@@ -56,6 +59,7 @@ xlabel('Time (s)')
 ylabel('Z Position (m)')
 legend('Aerospace Vehicle','Target','Location','northeastoutside')
 
+% plotting the 3-2-1 euler angles from the data in the e-frame
 figure
 sgtitle('3-2-1 Euler Angles vs Time for Both Objects in the E-Frame')
 
@@ -91,16 +95,24 @@ legend('Aerospace Vehicle','Target','Location','northeastoutside')
 
 %% Question 5
 
+% using the 3-2-1 given angles from the data and making a dcm and then
+% getting the 3-1-3 angles from the dcm 
+
+% need for loops to find the dcm and angles at each time step.
+
+% av angles
 for i = 1:length(av_att)
     av_DCM = RotationMatrix321(av_att(:,i));
     av_att_313(:,i) = EulerAngles313(av_DCM);
 end
 
+% target angles
 for i = 1:length(tar_att)
     tar_DCM = RotationMatrix321(tar_att(:,i));
     tar_att_313(:,i) = EulerAngles313(tar_DCM);
 end
 
+% plotting the euler angles found above
 figure
 sgtitle('3-1-3 Euler Angles vs Time for Both Objects in the E-Frame')
 
@@ -136,8 +148,11 @@ legend('Aerospace Vehicle','Target','Location','northeastoutside')
 
 %% Question 6 
 
+% the target position relative to the av in e is the difference between
+% their positions in e
 tar_pos_rel_E = tar_pos_inert - av_pos_inert;
 
+% plotting the position vector above 
 figure
 sgtitle('Position vs Time for the Target Relative to the Aerospace Vehicle in the E-Frame')
 
@@ -161,11 +176,14 @@ ylabel('Z Position (m)')
 
 %% Question 7
 
+% finding the dcm of the inertial frame to the body frame so we can covert
+% the vector found in 6. to body frame coordinates
 for i = 1:length(av_att)
     av_DCM = RotationMatrix321(av_att(:,i));
     tar_pos_rel_B(:,i) = av_DCM*tar_pos_rel_E(:,i);
 end
 
+% plotting the relative position in body coordinates
 figure
 sgtitle('Position vs Time for the Target Relative to the Aerospace Vehicle in the B-Frame')
 
@@ -189,15 +207,21 @@ ylabel('Z Position (m)')
 
 %% Question 8
 
+% finding the dcm from body coordinates to frame target body coordinates
+% and then finding the 3-2-1 euler angles of the dcm
+
+% the a to b dcm is found by the b to e dcm times the e to a dcm
+% and the e to b dcm is the inverse of the b to e dcm 
 for i = 1:length(av_att)
     av_DCM = RotationMatrix321(av_att(:,i));
     tar_DCM = RotationMatrix321(tar_att(:,i));
 
-    DCM_B_to_A = av_DCM\tar_DCM;
+    DCM_B_to_A = inv()*tar_DCM;
 
     B_to_A_att_321(:,i) = EulerAngles321(DCM_B_to_A);
 end
 
+% graphing the 3-2-1 angles found above from the b to a dcm 
 figure
 sgtitle('3-2-1 Euler Angles vs Time for B to A DCM')
 
